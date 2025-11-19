24e712291db0b01fcbea8eb5b05f9ef70419accb
P SRI HARSHITHA <2300032723@kluniversity.in>
2025-11-19 23:41:22 +0530

    added github actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // you can change this if needed
-  },
-})
+  base: "/TravelForge-fe_actions/",   // <-- ADD THIS (repo name)
+});
